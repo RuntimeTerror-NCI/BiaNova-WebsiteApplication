495d1b0969cc15a55f271766ccfc5a16abc0a6fb
Douglas Tiepolo <dvtiepolo@gmail.com>
2022-06-28 23:53:52 +0100

    animation added

--- src/UI/Backdrop.js
+++ src/UI/Backdrop.js
@@ -1,8 +1,17 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 import { Button } from './Button.styles';
 
 function Backdrop({ ...props }) {
+	const expandingAnimation = keyframes`
+	0% {opacity: 0}
+	20% {opacity:0.2}
+	40% {opacity:0.4}
+	60% {opacity: 0.6}
+	80% {opacity: 0.8}
+	100% {opacity: 1}
+	`;
+
 	const Backdrop = styled.div`
 		background-color: white;
 		height: 100vh;
@@ -12,6 +21,8 @@ function Backdrop({ ...props }) {
 		top: 50%;
 		left: 50%;
 		transform: translate(-50%, -50%);
+		animation-name: ${expandingAnimation};
+		animation-duration: 0.2s;
 	`;
 
 	const Header = styled.div``;
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,8 +1,17 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 import Backdrop from '../../UI/Backdrop';
 
 function Modal({ ...props }) {
+	const expandingAnimation = keyframes`
+	0% {opacity: 0}
+	20% {opacity:0.2}
+	40% {opacity:0.4}
+	60% {opacity: 0.6}
+	80% {opacity: 0.8}
+	100% {opacity: 1}
+	`;
+
 	const Wrapper = styled.div`
 		display: flex;
 		flex-direction: column;
@@ -14,6 +23,8 @@ function Modal({ ...props }) {
 		top: 50%;
 		left: 50%;
 		transform: translate(-50%, -50%);
+		animation-name: ${expandingAnimation};
+		animation-duration: 0.2s;
 	`;
 
 	return (
