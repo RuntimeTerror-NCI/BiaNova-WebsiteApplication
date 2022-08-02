b7645524046c00068b16a0d5bc768822fc27ab87
Douglas Tiepolo <dvtiepolo@gmail.com>
2022-08-02 19:15:06 +0100

    favorites added

--- src/components/Dashboard/EditProfile.js
+++ src/components/Dashboard/EditProfile.js
@@ -1,165 +1,101 @@
-import { Formik } from "formik";
-import { useState } from "react";
-import * as Yup from "yup";
-import axios from "axios";
-import {
-  Input,
-  StyledInlineErrorMessage,
-  InputField,
-  Wrapper,
-  StyledForm,
-  Frame,
-} from "../../UI/StyledForm";
+import { Formik } from 'formik';
+import { useState, useEffect } from 'react';
+import * as Yup from 'yup';
+import axios from 'axios';
+import { PillButton } from '../../UI/Button.styles';
+import { Input, InputField, Wrapper, StyledForm, Frame } from '../../UI/StyledForm';
+import Error from '../../UI/Error';
+import Spinner from '../../UI/Spinner';
+import styled from 'styled-components';
 
 const validationSchema = Yup.object().shape({
-  email: Yup.string()
-    .email("This email is not valid")
-    .required("Please enter your email")
-    .trim(),
-  password: Yup.string()
-    .required("Please enter your password")
-    .min(6, "Your password is too short, it should have at least 6 characters")
-    .matches(
-      /^(?=.*?[A-Za-z])(?=.*?[0-9]).{6,}$/,
-      "Password must contain at least one uppercase letter and one number"
-    )
-    .trim(),
+	email: Yup.string().email('This email is not valid').required('Please enter your email').trim(),
+	password: Yup.string()
+		.required('Please enter your password')
+		.min(6, 'Your password is too short, it should have at least 6 characters')
+		.matches(
+			/^(?=.*?[A-Za-z])(?=.*?[0-9]).{6,}$/,
+			'Password must contain at least one uppercase letter and one number'
+		)
+		.trim(),
 });
 
 function EditProfile({ user, token }) {
-  const [errMessage, setErrMessage] = useState("");
-  const [userValues, setUserValues] = useState({
-    email: "",
-    password: "",
-  });
+	const [errMessage, setErrMessage] = useState('');
 
-  const updatedUserValues = (values) => {
-    const newValues = values;
-    setUserValues(newValues);
-  };
+	useEffect(() => {
+		getUser();
+	}, []);
 
-  return (
-    <Frame>
-      <Wrapper>
-        <Error errMsg={errMessage} />
-        <Formik
-          initalValues={userValues}
-          validationSchema={validationSchema}
-          onSubmit={(values, actions) => {
-            updatedUserValues(values);
+	const getUser = () => {
+		axios.get('https://bianova.herokuapp.com/profile', {
+			username: user,
+			headers: {
+				'Authorization': `Bearer ${token}`,
+			},
+		});
+	};
 
-            axios
-              .put("https://bianova.herokuapp.com/prrofile", {
-                email: values.email,
-                password: values.password,
-              })
-              .then((response) => {
-                console.log(response);
-              })
-              .catch((error) => {
-                console.log(error);
-              })((error) => {
-              setErrMessage(error);
-            });
+	return (
+		<Frame>
+			<Wrapper>
+				<Error errMsg={errMessage} />
+				<Formik
+					validationSchema={validationSchema}
+					onSubmit={(values, actions) => {
+						axios
+							.put('https://bianova.herokuapp.com/profile', {
+								email: values.email,
+								password: values.password,
+							})
+							.then(response => {
+								console.log(response);
+							})
+							.catch(error => {
+								console.log(error);
+							})(error => {
+							setErrMessage(error);
+						});
 
-            const timeOut = setTimeout(() => {
-              actions.setSubmitting(false);
+						const timeOut = setTimeout(() => {
+							actions.setSubmitting(false);
 
-              clearTimeout(timeOut);
-            }, 1000);
-          }}
-        >
-          {({ handleSubmit, isSubmitting, isValid }) => {
-            return (
-              <>
-                <StyledForm
-                  name="edit-profile"
-                  method="put"
-                  onSubmit={handleSubmit}
-                >
-                  <InputField>
-                    <Label htmlFor="username">Username</Label>
-                    <Input
-                      type="text"
-                      name="username"
-                      autoCapitalize="off"
-                      autoCorrect="off"
-                      valid={touched.username && !errors.username}
-                      error={touched.username && errors.username}
-                    />
+							clearTimeout(timeOut);
+						}, 1000);
+					}}>
+					{({ handleSubmit, isSubmitting, isValid }) => {
+						return (
+							<>
+								<StyledForm name='edit-profile' method='put' onSubmit={handleSubmit}>
+									<InputField>
+										<Label htmlFor='email'>Email</Label>
+										<Input />
+									</InputField>
 
-                    {errors.username && touched.username && (
-                      <StyledInlineErrorMessage>
-                        {errors.username}
-                      </StyledInlineErrorMessage>
-                    )}
-                  </InputField>
+									<InputField>
+										<Label htmlFor='password'>Password</Label>
+										<Input />
+									</InputField>
 
-                  <InputField>
-                    <Label htmlFor="email">Email</Label>
-                    <Input
-                      type="email"
-                      name="email"
-                      autoCapitalize="off"
-                      autoCorrect="off"
-                      autoComplete="email"
-                      valid={touched.email && !errors.email}
-                      error={touched.email && errors.email}
-                    />
+									<InputField>
+										<Label htmlFor='confirmPassword'>Confirm Password</Label>
+										<Input />
+									</InputField>
 
-                    {errors.email && touched.email && (
-                      <StyledInlineErrorMessage>
-                        {errors.email}
-                      </StyledInlineErrorMessage>
-                    )}
-                  </InputField>
-
-                  <InputField>
-                    <Label htmlFor="password">Password</Label>
-                    <Input
-                      type="password"
-                      name="password"
-                      autoCapitalize="off"
-                      autoCorrect="off"
-                      valid={touched.password && !errors.password}
-                      error={touched.password && errors.password}
-                    />
-
-                    {errors.password && touched.password && (
-                      <StyledInlineErrorMessage>
-                        {errors.password}
-                      </StyledInlineErrorMessage>
-                    )}
-                  </InputField>
-
-                  <InputField>
-                    <Label htmlFor="confirmPassword">Confirm Password</Label>
-                    <Input
-                      type="password"
-                      name="confirmPassword"
-                      autoCapitalize="off"
-                      autoCorrect="off"
-                      valid={touched.confirmPassword && !errors.confirmPassword}
-                      error={touched.confirmPassword && errors.confirmPassword}
-                    />
-
-                    {errors.confirmPassword && touched.confirmPassword && (
-                      <StyledInlineErrorMessage>
-                        {errors.confirmPassword}
-                      </StyledInlineErrorMessage>
-                    )}
-                  </InputField>
-
-                  <PillButton type="submit" disabled={!isValid || isSubmitting}>
-                    {isSubmitting ? `Submitting...` : `Submit`}
-                  </PillButton>
-                  {isSubmitting && <Spinner />}
-                </StyledForm>
-              </>
-            );
-          }}
-        </Formik>
-      </Wrapper>
-    </Frame>
-  );
+									<PillButton type='submit' disabled={!isValid || isSubmitting}>
+										{isSubmitting ? `Submitting...` : `Submit`}
+									</PillButton>
+									{isSubmitting && <Spinner />}
+								</StyledForm>
+							</>
+						);
+					}}
+				</Formik>
+			</Wrapper>
+		</Frame>
+	);
 }
+
+const Label = styled.label``;
+
+export default EditProfile;
--- src/components/Pages/Dashboard.js
+++ src/components/Pages/Dashboard.js
@@ -1,43 +1,51 @@
-import { useNavigate } from "react-router-dom";
-import { PillButton } from "../../UI/Button.styles";
-import styled from "styled-components";
+import { useNavigate } from 'react-router-dom';
+import { PillButton } from '../../UI/Button.styles';
+import styled from 'styled-components';
+import EditProfile from '../Dashboard/EditProfile';
 
 function Dashboard({ user, token }) {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const logout = () => {
-    sessionStorage.clear();
-    navigate("/");
-    window.location.reload();
-  };
+	const logout = () => {
+		sessionStorage.clear();
+		navigate('/');
+		window.location.reload();
+	};
 
-  return (
-    <DashboardCon>
-      <h1>Hi there, {user} </h1>
-      <PillButton onClick={() => logout()}>Logout</PillButton>
-      <h2>Favourites</h2>
-    </DashboardCon>
-  );
+	return (
+		<DashboardCon>
+			<Header>
+				<h1>Hi there, {user} </h1>
+				<PillButton onClick={() => logout()}>Logout</PillButton>
+			</Header>
+			<EditProfile user={user} token={token} />
+			<h2>Favourites</h2>
+		</DashboardCon>
+	);
 }
 
 const DashboardCon = styled.div`
-  font-weight: 300;
-  margin-right: 4vw;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  flex: 1 0 auto;
+	font-weight: 300;
+	display: flex;
+	margin: auto;
+	flex-direction: column;
+	justify-content: center;
+	flex: 1 0 auto;
+`;
 
-  h1 {
-    font-family: "Poppins", sans-serif;
-    font-size: 4rem;
-    margin-top: 3rem;
-    margin-bottom: 1.2rem;
-    letter-spacing: 0.1rem;
-    line-height: 4.5rem;
-    text-align: center;
-  }
+const Header = styled.div`
+	display: flex;
+	width: 100%;
+
+	h1 {
+		font-family: 'Poppins', sans-serif;
+		font-size: 2rem;
+		margin-top: 3rem;
+		margin-bottom: 1.2rem;
+		letter-spacing: 0.1rem;
+		line-height: 4.5rem;
+		text-align: center;
+	}
 `;
 
 export default Dashboard;
