db31a0af56b5dd151ddbead8b24162c33ffbf1f1
CheezyDevs <leecampbellbusiness@icloud.com>
2022-08-05 19:16:08 +0100

    updates
    - cards not working in favorites..
    
    use effect issue - will need to have a look tomorrow,
    
    needs media queries
    
    needs the password reset page + testing (implemented forgotPassword)

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,203 +1,205 @@
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
-import { useState, useEffect, memo } from 'react';
-import LikePic from '../../assets/imgs/Like.png';
-import UnlikePic from '../../assets/imgs/Unlike.png';
-import axios from 'axios';
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { useState, useEffect, memo } from "react";
+import LikePic from "../../assets/imgs/Like.png";
+import UnlikePic from "../../assets/imgs/Unlike.png";
+import axios from "axios";
 
 function Card({ img, title, id, token, user }) {
-	const [like, setLike] = useState(false);
-	const [recipe, setRecipe] = useState();
-	const [favourites, setFavourites] = useState([]);
+  const [like, setLike] = useState(false);
+  const [recipe, setRecipe] = useState();
+  const [favourites, setFavourites] = useState([]);
 
-	const updateFavourites = () => {
-		const favs = JSON.parse(localStorage.getItem('favourites'));
-		setFavourites(favs);
-	};
+  const updateFavourites = () => {
+    const favs = JSON.parse(localStorage.getItem("favourites"));
+    setFavourites(favs);
+  };
 
-	let url = 'https://bianova.herokuapp.com/save';
+  let url = "https://bianova.herokuapp.com/save";
 
-	let headers = {
-		Authorization: `Bearer ${token}`,
-	};
+  let headers = {
+    Authorization: `Bearer ${token}`,
+  };
 
-	const addFavorite = () => {
-		const recipe = { id: id, title: title, img: img };
-		setRecipe(recipe);
-		let data = {
-			username: { username: user },
-			recipe: recipe,
-		};
+  const addFavorite = () => {
+    const recipe = { id: id, title: title, img: img };
+    setRecipe(recipe);
+    let data = {
+      recipe: recipe,
+    };
 
-		axios
-			.post(url, data, {
-				headers: headers,
-			})
-			.then(response => {
-				console.log(response);
-			})
-			.catch(error => {
-				console.log(error);
-			});
-	};
+    axios
+      .post(url, data, {
+        headers: headers,
+      })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-	const removeFavorite = () => {
-		const recipe = { id: id, title: title, img: img };
-		setRecipe(recipe);
+  const removeFavorite = () => {
+    const recipe = { id: id, title: title, img: img };
+    setRecipe(recipe);
 
-		let data = {
-			username: { username: user },
-			recipe: recipe,
-		};
+    let data = {
+      recipeID: { recipe: id },
+      recipe: recipe,
+    };
 
-		axios
-			.get('https://bianova.herokuapp.com/save', {
-				params: {
-					username: user,
-				},
-				headers: headers,
-			})
-			.then(response => {
-				console.log(response);
-			})
-			.catch(error => {
-				console.log(error);
-			});
+    let headers = {
+      Authorization: `Bearer ${token}`,
+    };
 
-		console.log('remove ', recipe);
-	};
+    let url = "https://bianova.herokuapp.com/delete_recipes";
 
-	const handleLove = () => {
-		setLike(prev => !prev);
-		if (!like) {
-			addFavorite();
-		} else {
-			removeFavorite();
-		}
-	};
+    axios
+      .delete(url, data, {
+        headers: headers,
+      })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
 
-	useEffect(() => {
-		if (localStorage.getItem('favourites') !== null) {
-			localStorage.removeItem('favourites');
-		}
+    console.log("remove ", recipe);
+  };
 
-		if (token) {
-			let url = 'https://bianova.herokuapp.com/profile';
-			let headers = {
-				Authorization: `Bearer ${token}`,
-			};
+  const handleLove = () => {
+    setLike((prev) => !prev);
+    if (!like) {
+      addFavorite();
+    } else {
+      removeFavorite();
+    }
+  };
 
-			axios
-				.get(url, {
-					params: {
-						username: user,
-					},
-					headers: headers,
-				})
+  useEffect(() => {
+    if (localStorage.getItem("favourites") !== null) {
+      localStorage.removeItem("favourites");
+    }
 
-				.then(response => {
-					let savedRecipes = response.data['savedRecipesObjects'];
+    if (token) {
+      let url = "https://bianova.herokuapp.com/profile";
+      let headers = {
+        Authorization: `Bearer ${token}`,
+      };
 
-					localStorage.setItem('favourites', JSON.stringify(savedRecipes));
+      axios
+        .get(url, {
+          params: {
+            username: user,
+          },
+          headers: headers,
+        })
 
-					for (let i = 0; i < savedRecipes?.length; i++) {
-						if (savedRecipes[i].id === id) {
-							setLike(true);
-						}
-					}
-				})
-				.catch(error => {
-					console.log(error);
-				});
-		}
+        .then((response) => {
+          let savedRecipes = response.data["savedRecipesObjects"];
 
-		updateFavourites();
-	}, [id]);
+          localStorage.setItem("favourites", JSON.stringify(savedRecipes));
 
-	return (
-		<CardContainer>
-			<ImageContainer>
-				<img src={img} alt={title} />
-			</ImageContainer>
+          for (let i = 0; i < savedRecipes?.length; i++) {
+            if (savedRecipes[i].id === id) {
+              setLike(true);
+            }
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
 
-			<CardContent>
-				<h3>{title} </h3>
-				<LikeWrapper onClick={() => handleLove()}>
-					{like ? <Img src={LikePic} /> : <Img src={UnlikePic} />}
-				</LikeWrapper>
+    updateFavourites();
+  }, [id]);
 
-				<View to={`/recipe/${id}`}>View Recipe</View>
-			</CardContent>
-		</CardContainer>
-	);
+  return (
+    <CardContainer>
+      <ImageContainer>
+        <img src={img} alt={title} />
+      </ImageContainer>
+
+      <CardContent>
+        <h3>{title} </h3>
+        <LikeWrapper onClick={() => handleLove()}>
+          {like ? <Img src={LikePic} /> : <Img src={UnlikePic} />}
+        </LikeWrapper>
+
+        <View to={`/recipe/${id}`}>View Recipe</View>
+      </CardContent>
+    </CardContainer>
+  );
 }
 
 const View = styled(Link)`
-	display: flex;
-	margin: auto;
-	margin-bottom: 1rem;
-	padding: 0.2rem 0.8rem;
-	align-items: center;
-	text-decoration: none;
-	background-color: #b8e2ff;
-	width: fit-content;
-	height: 2rem;
-	cursor: pointer;
-	font-weight: 500;
-	color: black;
-	transition: all 0.2s ease-in-out;
-	&:hover {
-		background-color: #96d5ff;
-	}
+  display: flex;
+  margin: auto;
+  margin-bottom: 1rem;
+  padding: 0.2rem 0.8rem;
+  align-items: center;
+  text-decoration: none;
+  background-color: #b8e2ff;
+  width: fit-content;
+  height: 2rem;
+  cursor: pointer;
+  font-weight: 500;
+  color: black;
+  transition: all 0.2s ease-in-out;
+  &:hover {
+    background-color: #96d5ff;
+  }
 `;
 const LikeWrapper = styled.div`
-	width: fit-content;
-	display: flex;
-	margin: 0.8rem;
-	cursor: pointer;
+  width: fit-content;
+  display: flex;
+  margin: 0.8rem;
+  cursor: pointer;
 `;
 
 const Img = styled.img`
-	width: 2.5rem;
+  width: 2.5rem;
 `;
 
 const CardContainer = styled.div`
-	justify-content: center;
-	margin: 2rem;
-	width: 18rem;
-	display: flex;
-	height: 25rem;
-	flex-direction: column;
-	border-radius: 0.2rem;
-	box-shadow: 0px 0px 15px 0px;
-	transition: 0.3s;
-	animation: ease-in;
-	margin: 3rem 1.5rem;
-	background-color: whitesmoke;
-	&:hover {
-		transform: scale(1.1);
-		box-shadow: 0px 0px 15px 0px;
-	}
+  justify-content: center;
+  margin: 2rem;
+  width: 18rem;
+  display: flex;
+  height: 25rem;
+  flex-direction: column;
+  border-radius: 0.2rem;
+  box-shadow: 0px 0px 15px 0px;
+  transition: 0.3s;
+  animation: ease-in;
+  margin: 3rem 1.5rem;
+  background-color: whitesmoke;
+  &:hover {
+    transform: scale(1.1);
+    box-shadow: 0px 0px 15px 0px;
+  }
 `;
 
 const CardContent = styled.div`
-	margin: 1rem;
-	margin-top: 0.3rem;
-	h3 {
-		margin-bottom: 0.5rem;
-		text-align: center;
-		font-size: 1.2rem;
-	}
+  margin: 1rem;
+  margin-top: 0.3rem;
+  h3 {
+    margin-bottom: 0.5rem;
+    text-align: center;
+    font-size: 1.2rem;
+  }
 `;
 
 const ImageContainer = styled.div`
-	img {
-		width: 100%;
-		height: 10rem;
-		object-position: 15% 75%;
-		object-fit: cover;
-		border-top-left-radius: 0.2rem;
-		border-top-right-radius: 0.2rem;
-	}
+  img {
+    width: 100%;
+    height: 10rem;
+    object-position: 15% 75%;
+    object-fit: cover;
+    border-top-left-radius: 0.2rem;
+    border-top-right-radius: 0.2rem;
+  }
 `;
 export default memo(Card);
--- src/components/UserLinks/ForgotPassword.js
+++ src/components/UserLinks/ForgotPassword.js
@@ -1,69 +1,123 @@
-import styled from 'styled-components';
-import React, { useState } from 'react';
-import { Input, StyledInlineErrorMessage, InputField, Wrapper } from '../../UI/StyledForm';
-import { PillButton } from '../../UI/Button.styles';
-import { Formik, Form, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
+import styled from "styled-components";
+import React, { useState } from "react";
+import {
+  Input,
+  StyledInlineErrorMessage,
+  InputField,
+  Wrapper,
+  StyledForm,
+} from "../../UI/StyledForm";
+import { useNavigate } from "react-router-dom";
+import { PillButton } from "../../UI/Button.styles";
+import { Formik, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+import Spinner from "../../UI/Spinner";
+import Error from "../../UI/Error";
 
 const Label = styled.label``;
 
 const validationScehma = Yup.object().shape({
-	email: Yup.string().email('Email was not found').required('Please Enter A Valid Email'),
+  username: Yup.string().required("Please enter your username").trim(),
 });
 
-function ForgotPassword() {
-	const [formValues, setFormValues] = useState();
+function ForgotPassword(token, user) {
+  const [formValues, setFormValues] = useState({
+    username: "",
+  });
 
-	return (
-		<Wrapper>
-			<Formik
-				initialValues={{
-					email: '',
-				}}
-				validationSchema={validationScehma}
-				onSubmit={(values, actions) => {
-					console.log(values);
-					setFormValues(values);
+  const updatedFormValues = (values) => {
+    const newValues = values;
+    setFormValues(newValues);
+  };
 
-					const timeOut = setTimeout(() => {
-						actions.setSubmitting(false);
+  const [errMessage, setErrMessage] = useState("");
 
-						clearTimeout(timeOut);
-					}, 1000);
-				}}>
-				{({ values, errors, touched, handleSubmit, isSubmitting, isValidating, isValid }) => {
-					return (
-						<>
-							<Form>
-								<InputField>
-									<Label htmlFor='email'>
-										Email
-										<Input
-											type='email'
-											name='email'
-											autoCapitalize='off'
-											autoCorrect='off'
-											autoComplete='email'
-											placeholder='your email'
-											valid={touched.email && !errors.email}
-											error={touched.email && errors.email}
-										/>
-									</Label>
-									<ErrorMessage name='email'>
-										{msg => <StyledInlineErrorMessage> {msg} </StyledInlineErrorMessage>}
-									</ErrorMessage>
-								</InputField>
+  const navigate = useNavigate();
 
-								<PillButton type='submit' disabled={!isValid || isSubmitting}>
-									{isSubmitting ? `Submitting...` : `Submit`}
-								</PillButton>
-							</Form>
-						</>
-					);
-				}}
-			</Formik>
-		</Wrapper>
-	);
+  const handleRedirect = () => {
+    navigate("/");
+  };
+
+  return (
+    <Wrapper>
+      <Error errMsg={errMessage} />
+      <Formik
+        initialValues={formValues}
+        validationSchema={validationScehma}
+        onSubmit={(values, actions) => {
+          updatedFormValues(values);
+          let headers = {
+            Authorization: `Bearer ${token}`,
+          };
+
+          axios
+            .get("https://bianova.herokuapp.com/reset_email", {
+              params: {
+                username: values.username,
+              },
+              headers: headers,
+            })
+            .then((response) => {
+              console.log(response);
+              handleRedirect();
+              alert("Please Check Your Email For Reset Code!");
+            })
+            .catch((err) => {
+              console.log(err);
+              setErrMessage("something went wrong...");
+            });
+
+          const timeOut = setTimeout(() => {
+            actions.setSubmitting(false);
+
+            clearTimeout(timeOut);
+          }, 1000);
+        }}
+      >
+        {({ errors, touched, handleSubmit, isSubmitting, isValid }) => {
+          return (
+            <>
+              <StyledForm
+                name="forgot-password"
+                method="get"
+                onSubmit={handleSubmit}
+              >
+                <InputField>
+                  <Label htmlFor="username">
+                    Email
+                    <Input
+                      type="username"
+                      name="username"
+                      autoCapitalize="off"
+                      autoCorrect="off"
+                      autoComplete="off"
+                      placeholder="Enter Username"
+                      valid={touched.username && !errors.username}
+                      error={touched.username && errors.username}
+                    />
+                  </Label>
+                  <ErrorMessage name="username">
+                    {(msg) => (
+                      <StyledInlineErrorMessage>
+                        {" "}
+                        {msg}{" "}
+                      </StyledInlineErrorMessage>
+                    )}
+                  </ErrorMessage>
+                </InputField>
+
+                <PillButton type="submit" disabled={!isValid || isSubmitting}>
+                  {isSubmitting ? `Submitting...` : `Submit`}
+                </PillButton>
+                {isSubmitting && <Spinner />}
+              </StyledForm>
+            </>
+          );
+        }}
+      </Formik>
+    </Wrapper>
+  );
 }
 
 export default ForgotPassword;
